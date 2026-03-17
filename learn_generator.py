5eeae5b41d896f7dee540da6c37e91ef2f0f079a
Alexandre COSTE <coste.alexandre09@gmail.com>
2026-03-17 17:18:55 +0100

    Add exercise 2: read file by line and compare classic function to generator

--- learn_generator.py
+++ learn_generator.py
@@ -1,5 +1,7 @@
 """Exercises to learn decorators"""
 
+from pathlib import Path
+
 # =================================
 # Exercise 1: Fibonacci generator
 # =================================
@@ -12,6 +14,26 @@ def fibonacci():
         n, m = m, n + m
         print(n, m)
 
+
+# =========================================
+# Exercise 2: Read file by line generator
+# =========================================
+
+def file_by_line(path="docs/pipeline.log"): # Generator read the file line by line
+    """Generator to read a file line by line and save RAM use"""
+    path = Path(path) # Define path
+    
+    with path.open(mode="r") as file:
+        for line in file:
+            yield line # Line lazy evaluation
+
+def file_readlines(path="docs/pipeline.log"): # Load the data directly in Memory
+    """Function that load the entire file in RAM to compare with the generator"""
+    path = Path(path) # Define path
+    with path.open(mode="r") as file:
+        lines = file.readlines() # Get all line in the file
+        for line in lines:
+            print(line)
 
 # ===================================
 # TESTS
