96f51ee14b361eb586cc99fb7d4c6db4b4e5b648
Alexandre COSTE <coste.alexandre09@gmail.com>
2026-03-23 20:48:57 +0100

    Add exercise 2: make post request on JSONPlaceholder posts

--- learn_requests.py
+++ learn_requests.py
@@ -2,9 +2,9 @@
 
 import requests, json
 
-# ==================================================
-# Exercise 1: Get request on JSONPlaceholder posts
-# ==================================================
+# ==================================================================
+# Exercise 1: Get requests on JSONPlaceholder posts, users & todos
+# ==================================================================
 
 response_posts = requests.get('https://jsonplaceholder.typicode.com/posts') # Get all the posts
 response_users = requests.get('https://jsonplaceholder.typicode.com/users') # Get all the users
@@ -22,3 +22,21 @@ print(f"Status code: {response_todos.status_code}\n") # Display the code status
 print(f"Header:\n{json.dumps(dict(response_todos.headers), indent=4)}\n") # Display the request header
 print(f"First todo:\n{json.dumps(response_todos.json()[0], indent=4)}") # Get the first todo
 
+
+# ==================================================
+# Exercise 2: Post request on JSONPlaceholder posts
+# ==================================================
+
+# Define the post to post on JSONPlaceholder
+post = {
+    "userId": 199681,
+    "id": 199681,
+    "title": "requests learning",
+    "body": "A simple test to learn how to use requests librairy"
+}
+
+# Post request to post a new post
+request_post = requests.post('https://jsonplaceholder.typicode.com/posts', json=post)
+
+print(f"\nPost request status: {request_post.status_code}") # Check the status code
+print(f"Response body:\n{json.dumps(request_post.json(), indent=4)}") # Display the response body
