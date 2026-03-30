741511d5f981e629fd8b004fad4866b7dac52bd7
Alexandre COSTE <coste.alexandre09@gmail.com>
2026-03-30 20:59:15 +0200

    Add exercises to learn scraping

---
+++ learn_scraping.py
@@ -0,0 +1,21 @@
+"""Exercises to learn decorators"""
+
+import requests
+from bs4 import BeautifulSoup
+
+# ==================================================================
+# Exercise 1: Scrape first page of http://books.toscrape.com/
+# and display title, price, availability and rate for each book 
+# ==================================================================
+
+response = requests.get("http://books.toscrape.com/")
+print(f"Response status: {response.status_code}\n")
+html = response.text
+soup = BeautifulSoup(html, "html.parser")
+
+books = soup.find_all("li", class_=True)
+#for book in books:
+ #   print(book.get_text())
+
+print(books)
+#print(soup.prettify())
